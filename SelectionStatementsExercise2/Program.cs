c6d0167e31b2c3d9731d136ad0720d2060b60839
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-11-29 10:21:38 -0700

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Reflection.Metadata;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            
+            string favsub = Console.ReadLine();
+
+            switch (favsub.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is the best!");
+                    break;
+                case "biology":
+                    Console.WriteLine("Biology rocks!");
+                    break;
+                case "reading":
+                    Console.WriteLine("I love to read!");
+                    break;
+                case "writing":
+                    Console.WriteLine("Writing, I love telling stories!");
+                    break;
+                case "gym":
+                    Console.WriteLine("I crush it in the Gym");
+                    break;
+                default:
+                    Console.WriteLine("It's complicated");
+                    break;
+
+
+            }
         }
     }
 }
